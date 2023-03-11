3ec9c970e2b94f6d655a3a839cef2203bd56b22d
Tariyel <islami.tariyel@gmail.com>
2023-03-11 14:31:30 +0400

    Homeview added

--- SwiftUI_Twitter/Presentation/Views/Home/HomeView.swift
+++ SwiftUI_Twitter/Presentation/Views/Home/HomeView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            ForEach(0...20, id: \.self){ _ in
+                TweetsRowView().padding(.horizontal)
+                Divider()
+
+            }
+        }
     }
 }
 
