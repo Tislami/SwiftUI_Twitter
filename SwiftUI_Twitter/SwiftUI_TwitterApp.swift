270b566782b19a13d6b3f975ccfd6a9fb4809bc7
Tariyel <islami.tariyel@gmail.com>
2023-03-11 14:15:07 +0400

    Initial commit

---
+++ SwiftUI_Twitter/SwiftUI_TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TwitterApp.swift
+//  SwiftUI_Twitter
+//
+//  Created by Tariyel Islami on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
